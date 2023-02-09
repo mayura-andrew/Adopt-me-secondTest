9123a4f9881e5c1770f18b4895f733e2df14aa57
mayura_andrew <48531182+mayura-alahakoon@users.noreply.github.com>
2023-02-10 02:51:29 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+const App = () => {
+    return (
+        <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+        <header>
+            <h1>Adopt Me!</h1>
+
+        </header>
+            <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+            </Routes>
+        </QueryClientProvider>
+        </BrowserRouter>
+    
+    
+
+    );
+};
+
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
+root.render(<App />);
