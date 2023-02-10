e11086b18c22e07f063b45fc79358ec3cf69617b
mayura_andrew <mayura.a@parallax.lk>
2023-02-10 18:31:25 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Details from "./Details";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -11,10 +13,14 @@ const queryClient = new QueryClient({
     },
   },
 });
+
 const App = () => {
+    const adoptedPet = useState(null);
     return (
         <BrowserRouter>
         <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+
         <header>
             <h1>Adopt Me!</h1>
 
@@ -23,7 +29,9 @@ const App = () => {
                 <Route path="/details/:id" element={<Details />} />
                 <Route path="/" element={<SearchParams />} />
             </Routes>
+        </AdoptedPetContext.Provider>
         </QueryClientProvider>
+
         </BrowserRouter>
     
     
