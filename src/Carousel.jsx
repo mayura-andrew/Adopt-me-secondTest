8923b447d480ef186d3602b9068aa77a18058e90
mayura_andrew <mayura.a@parallax.lk>
2023-02-12 23:36:59 +0530

    need to write test

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -26,7 +26,7 @@ class Carousel extends Component {
         const { images } = this.props;
         return (
             <div className="carousel">
-                <img src={images[active]} alt="animal" />
+                <img data-testid="hero" src={images[active]} alt="animal" />
                 <div className="carousel-smaller">
                     {images.map((photo, index) => (
                         <img 
@@ -34,6 +34,7 @@ class Carousel extends Component {
                             data-index={index}
                             key={photo}
                             src={photo}
+                            data-testid={`thumbnail${index}`}
                             className={index === active ? "active" : ""}
                             alt= "animal thumbnail" />
 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    root: "src"
+    root: "src",
+    test: {
+        environment: "happy-dom",
+    }
 });
